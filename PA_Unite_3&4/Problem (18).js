417c28a0788c272f100e9c4fda117f8bf09649b6
imrajeevraj <rajeev.ranjan948@hotmail.com>
2025-12-30 00:13:37 +0530

    Delete PA_Unite_3&4 directory

--- PA_Unite_3&4/Problem (18).js
+++
@@ -1,6 +0,0 @@
-// Print Array Items
-
-let fruits = ["Apple", "Banana", "Mango"];
-for (let item of fruits) {
-    console.log(item);
-}
--- PA_Unite_3&4/Problem (19).js
+++
@@ -1,4 +0,0 @@
-let student = {Name:"Rajeev Ranjan", Age:29, Course:"CSE"};
-for (let key in student) {
-    console.log(key + ":", student[key]);
-}
